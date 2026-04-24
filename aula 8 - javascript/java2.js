2044941be23764442f3f7b0bd9a4b6bcd943508c
andreerfr <andreerci@gmail.com>
2026-04-24 10:52:03 -0300

    java2

---
+++ aula 8 - javascript/java2.js
@@ -0,0 +1,9 @@
+var a = 1;
+switch (a) {
+    case 1:
+        console.log("Opção 1");
+        case 2:
+            console.log("Opção 2");
+            default:
+                console.log("Nenhuma das opções anteriores");
+}
