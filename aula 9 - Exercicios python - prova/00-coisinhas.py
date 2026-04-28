a11692e4f60c65897aba97ee1e841377824ffb0c
andreerfr <andreerci@gmail.com>
2026-04-28 08:23:21 -0300

    coisinhas-python

---
+++ aula 9 - Exercicios python - prova/00-coisinhas.py
@@ -0,0 +1,12 @@
+# Criar variáveis e receber input do usuário
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+# Estrutura condicional (if/else)
+if idade >= 18:
+    print(f"{nome}, você é maior de idade.")
+else:
+    print(f"{nome}, você é menor de idade.")
+
+    # Exemplo: Olá Mundo
+print("Olá, Mundo!")
