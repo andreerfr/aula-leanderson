f9690aacd2382cad1998d7655fa68b91f10c7e04
andreerfr <andreerci@gmail.com>
2026-04-29 10:39:04 -0300

    arranjos

--- aula 10 - js/arranjo.js
+++ aula 10 - js/arranjo.js
@@ -2,3 +2,14 @@
 //variavel tem identificador
 //e armazena apenas uma imformação
 var i = 10;
+
+//arranjo é uma matriz d uma linha
+//1xN -> é a quantidade de colunas
+
+var arranjo = [9.5, 4.4, 10, 7]
+console.log(arranjo);
+console.log("Tamanho do arranjo:" + arranjo.length); //descobre o tamanho de um arranjo (quantidade de elementos nele)
+console.log("arranjo[0]: " +arranjo[0]); //a primeira posição do arranjo é 0
+console.log("arranjo[3]: " +arranjo[arranjo.lenght -1]); //a primeira posição do arranjo é -1
+arranjo [2] = -999; //modifica o elemento na posição 2
+console.log(arranjo); 
